08b3791e7751e3b65b160cca1e9ede1d22bbcfe5
Drew Dolgert <adolgert@uw.edu>
2020-02-16 11:48:35 -0800

    put data into the right directory for making a package

--- R/download.R
+++ R/download.R
@@ -1,10 +1,45 @@
 # Retrieve data for Bioko work
 library(curl)
 
-#
+
+#' Reads configuration file on where to download data.
+#' If you want to make the config file, then create
+#' a file called `$HOME/.config/MASH/data.ini` and put
+#' the following in it:
+#' ```
+#' [Default]
+#' SCPHOST = computer-name.ihme.uw.edu
+#' SCPHOSTBASE = /path/to/data/directory
+#' ```
+data_configuration <- function() {
+  home <- list(
+    xdg = Sys.getenv("XDG_CONFIG_HOME"),
+    env = fs::path(Sys.getenv("HOME"), ".config"),
+    default = fs::path("", "home", Sys.info()[["effective_user"]])
+  )
+
+  for (directory in home) {
+    ini_path <- fs::path(directory, "MASH", "data.ini")
+    if (file.exists(ini_path)) {
+      cfg <- configr::read.config(ini_path)
+      if (is.list(cfg)) {
+        data_configuration <<- cfg$Default
+        return(data_configuration)
+      }
+    }
+  }
+  FALSE
+}
+
+
 #' Unzip a 7zip file. There is a Github project called archive
 #' that would do this, too. This command is on Ubuntu but may
 #' not be elsewhere.
+#'
+#' @param archive Path to the file in 7z format.
+#' @param where Directory into which to unzip the archive.
+#'     This command changes the working directory to `where`
+#'     in order to unzip.
 un7zip <- function(archive, where) {
   archive <- normalizePath(archive)
   current_path <- setwd(where)
@@ -16,9 +51,11 @@ un7zip <- function(archive, where) {
 #' Use scp to retrieve data, if you have ssh credentials set up.
 #' This is used for data that isn't yet public.
 #' Equivalent to: ssh ihme.uw.edu:/path/to/file local_file.dat
-get_from_ihme <- function(filename, local_directory = "data") {
-  base_directory <- "/ihme/malaria_modeling/data/equatorial_guinea"
-  host <- "cluster-submit1.ihme.uw.edu"
+#'
+#' You have to call `data_configuration()` before you call this.
+get_from_ihme <- function(filename, local_directory = "inst/extdata") {
+  base_directory <- fs::path(data_configuration$SCPHOSTBASE, "equatorial_guinea")
+  host <- data_configuration$SCPHOST
   target <- fs::path(base_directory, filename)
   local_file <- fs::path(local_directory, filename)
   system(paste("scp", paste(host, target, sep = ":"), local_file, sep = " "))
@@ -30,7 +67,7 @@ get_from_ihme <- function(filename, local_directory = "data") {
 #' 10 or 15 is 2010 or 2015 data.
 #' adjv2 or v2 is whether it was adjusted to match WHO.
 #' So use GNQ15v2.tif.
-download_worldpop <- function(local_directory = "data", overwrite = FALSE) {
+download_worldpop <- function(local_directory = "inst/extdata", overwrite = FALSE) {
   local_file <- fs::path(local_directory, "Equatorial_Guinea_100m_Population.7z")
   worldpop_directory <- fs::path_ext_remove(local_file)
   remote_url <- "ftp://ftp.worldpop.org.uk/GIS/Population/Individual_countries/GNQ/Equatorial_Guinea_100m_Population.7z"
@@ -49,7 +86,7 @@ download_worldpop <- function(local_directory = "data", overwrite = FALSE) {
 #' Bioko grids are two shapefiles in UTM zone 32N projection.
 #' The 100m grids are secs.shp and the 1km are mapareas.shp.
 #' The two grids align in this projection.
-download_bioko_grids <- function(local_directory = "data") {
+download_bioko_grids <- function(local_directory = "inst/extdata") {
   filename <- "Bioko_grids.zip"
   local_path <- fs::path(local_directory, filename)
   destination_directory <- fs::path_ext_remove(local_path)
@@ -63,8 +100,8 @@ download_bioko_grids <- function(local_directory = "data") {
 }
 
 
-read_bioko_grids <- function(local_directory = "data") {
-  grid_dir <- fs::path("data", "Bioko_grids")
+read_bioko_grids <- function(local_directory = "inst/extdata") {
+  grid_dir <- fs::path(local_directory, "Bioko_grids")
   fine_grid <- sf::st_read(dsn = fs::path(grid_dir, "secs.shp"), layer = "secs")
   coarse_grid <- sf::st_read(dsn = fs::path(grid_dir, "mapareas_grid.shp"), layer = "mapareas_grid")
   list(fine = fine_grid, coarse = coarse_grid)
@@ -72,7 +109,7 @@ read_bioko_grids <- function(local_directory = "data") {
 
 
 #' HRSL is a geotiff in WGS 84 for all of Equatorial Guinea (GNQ).
-download_hrsl_points <- function(local_directory = "data") {
+download_hrsl_points <- function(local_directory = "inst/extdata") {
   filename <- "population_gnq_2018-10-01.zip"
   local_path <- fs::path(local_directory, filename)
   if (!file.exists(local_path)) {
@@ -82,6 +119,6 @@ download_hrsl_points <- function(local_directory = "data") {
 }
 
 
-read_hrsl <- function(local_directory = "data") {
+read_hrsl <- function(local_directory = "inst/extdata") {
   raster::raster(fs::path(local_directory, "population_gnq_2018-10-01.tif"))
 }
